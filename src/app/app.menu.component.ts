84aeb74696ce472c2e59c459ac7d362c33457a3a
cagatay.civici <cagatay.civici@fd9223c1-c77d-41e1-af93-17165896985c>
2018-08-15 11:37:43 +0000

    docs

--- src/app/app.menu.component.ts
+++ src/app/app.menu.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit, EventEmitter, ViewChild} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {trigger, state, style, transition, animate} from '@angular/animations';
 import {MenuItem} from 'primeng/primeng';
 import {AppComponent} from './app.component';
--- src/app/demo/view/documentation.component.ts
+++ src/app/demo/view/documentation.component.ts
@@ -6,8 +6,10 @@ import { BreadcrumbService } from '../../breadcrumb.service';
     styles: [`
         .docs pre.doc-command {
             font-family: monospace;
-            background-color: #EFEFEF;
+            background-color: #ffffff;
             color: #333333;
+            border: 1px solid #dfdfdf;
+            border-left: 10px solid #dae8ef;
             padding: 1em;
             font-size: 14px;
             border-radius: 0;
